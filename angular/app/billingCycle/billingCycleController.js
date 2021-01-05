24f9f6d40932b2fb9adaeca0698f346dc90b903c
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2021-01-04 21:59:50 -0300

    front end - create, update, delete, read

---
+++ angular/app/billingCycle/billingCycleController.js
@@ -0,0 +1,51 @@
+(function () {
+    angular.module('financeApp').controller('BillingCycleCtrl', [
+        '$http',
+        'msgs',
+        'tabs',
+        BillingCycleController
+    ])
+
+    function BillingCycleController($http, msgs, tabs) {
+        const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.refresh = function () {
+            $http.get(url).then(function (response) {
+                console.log(response)
+                vm.billingCycle = {}
+                vm.billingCycles = response.data
+                tabs.show(vm, {
+                    tabList: true,
+                    tabCreate: true
+                })
+            })
+        }
+
+        vm.create = function () {
+            $http.post(url, vm.billingCycle).then(function (response) {
+                vm.refresh()
+                msgs.addSuccess('Operação realizada com sucesso!')
+            }).catch(function (data) {
+                msgs.addError(data.errors)
+            })
+        }
+
+        vm.showTabUpdate = function (billingCycle) {
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {
+                tabUpdate: true
+            })
+        }
+
+        vm.showTabDelete = function (billingCycle) {
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {
+                tabDelete: true
+            })
+        }
+
+        vm.refresh()
+    }
+
+})()
---
+++ angular/app/common/components/field.js
@@ -0,0 +1,26 @@
+(function () {
+    angular.module('financeApp').component('field', {
+        bindings: {
+            id: '@',
+            label: '@',
+            grid: '@',
+            placeholder: '@',
+            type: '@',
+            model: '='
+        },
+        controller: [
+            'gridSystem',
+            function (gridSystem) {
+                this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
+            }
+        ],
+        template: `
+        <div class="{{ $ctrl.gridClasses }}">
+            <div class="form-group">
+                <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
+                <input type="{{ $ctrl.type }}" id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" ng-model="$ctrl.model">
+            </div>
+        </div>
+        `
+    })
+})()
